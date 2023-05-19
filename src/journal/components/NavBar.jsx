5cb876893f1daf3d264d36209508e71cccfc7fdd
sadys castro <sadyscastro@gmail.com>
2023-05-19 02:03:00 -0600

    creando carpeta dist

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -31,7 +31,7 @@ export const NavBar = ({ drawerWidth = 240 }) => {
                 </IconButton>
 
                 <Grid container direction='row' justifyContent='space-between' alignItems='center'>
-                    <Typography variant='h6' noWrap component='div'> JournalApp </Typography>
+                    <Typography variant='h6' noWrap component='div'> Diario de Notas </Typography>
 
                     <IconButton
                         color='error'
